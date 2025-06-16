382d415c71305f6a1cb3ed974690e09f16197383
infiniteluckyog <for.no.who@gmail.com>
2025-06-16 19:27:51 +0530

    Update fivedol.py

--- fivedol.py
+++ fivedol.py
@@ -208,5 +208,6 @@ def donate():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # required for Render
+    app.run(host="0.0.0.0", port=port)
   
